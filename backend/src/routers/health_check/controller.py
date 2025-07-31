344275a5117923fa4735b7d6a41f4214e6f545d0
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-07-31 20:17:13 +0530

    added rate limiting from health check module

--- backend/src/routers/health_check/controller.py
+++ backend/src/routers/health_check/controller.py
@@ -1,12 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 
+from ...rate_limiter import limiter
 from .models import HealthCheckResponse
 
 router = APIRouter(prefix="/api/v1/health-check", tags=["Health Check"])
 
 
 @router.get("/", summary="Health Check", response_model=HealthCheckResponse)
-async def health_check():
+@limiter.limit("100/minute")
+async def health_check(request: Request):
     """
     Endpoint to check the health of the API.
     Returns a simple status message.
