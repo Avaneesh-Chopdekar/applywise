b22bb15ca2312506f79147e20650b403d244dba8
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-07-31 10:23:58 +0530

    added ATSAnalysis document to mongodb

--- backend/src/database.py
+++ backend/src/database.py
@@ -3,6 +3,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from dotenv import load_dotenv
 
+
+from .routers.ats.models import ATSAnalysis
 from .routers.resumes.models import Resume
 
 load_dotenv()
@@ -12,6 +14,6 @@ async def init_db():
     client = AsyncIOMotorClient(os.getenv("MONGO_URI"))
     database = client.get_database("applywise")
 
-    await init_beanie(database, document_models=[Resume])
+    await init_beanie(database, document_models=[Resume, ATSAnalysis])
 
     return database
