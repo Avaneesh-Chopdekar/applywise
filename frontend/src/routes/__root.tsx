74fc67df54a44bb3397338ca7125fba196b4b127
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2025-07-31 21:16:08 +0530

    fixed @ alias imports from vite config and tsconfig

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
-import Provider from "../components/providers";
+import Provider from "@/components/providers";
 
 export const Route = createRootRoute({
   component: () => (
